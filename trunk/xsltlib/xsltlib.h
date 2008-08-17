47d3d0e4b75de9080ced069e935f843aad4dd6c6
smilingthax <smilingthax@c3327504-1e54-0410-be8d-d5a36682d75c>
2008-08-17 20:26:58 +0000

    added convenience include file

---
+++ trunk/xsltlib/xsltlib.h
@@ -0,0 +1,10 @@
+// combined include file
+
+#include "mine_ext/textItem.h"
+#include "mine_ext/extension.h"
+#include "split/split.h"
+#include "parset/parset.h"
+#include "zip_ext/zipext.h"
+#include "enclose/enclose.h"
+#include "imagetools/imagefun.h"
+
