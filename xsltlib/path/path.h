0771a7e04683c86597d68feb4e19d7e2a308834a
smilingthax <smilingthax@c3327504-1e54-0410-be8d-d5a36682d75c>
2010-10-07 23:57:37 +0000

    more spres support

--- xsltlib/path/path.h
+++ xsltlib/path/path.h
@@ -8,9 +8,9 @@ extern "C" {
 #endif
 
 int load_path();
-int path_register_static_file(const char *filename,const char *data,int len);
+int path_register_static_file(const char *filename,const char *data,int len); // i.e. "filename" contains (data,len)
 int path_register_remap(const char *filename,const char *path);
-int path_register_prefix(const char *prefix,const char *path);
+int path_register_prefix(const char *prefix,const char *path); // [prefix] will be substituted by path
 
 #ifdef __cplusplus
 };
