162aece7df2cf169aa08ee26a0581716c302632e
smilingthax <smilingthax@c3327504-1e54-0410-be8d-d5a36682d75c>
2011-11-06 09:45:07 +0000

    new file

---
+++ xmltools/xpath.h
@@ -0,0 +1,29 @@
+#ifndef _XPATH_H
+#define _XPATH_H
+
+#include <string>
+#include <libxml/tree.h>
+#include <libxml/xpath.h>
+
+class XPathContext {
+public: 
+  XPathContext(xmlDocPtr doc);
+  ~XPathContext();
+
+  void setVariable(const char *name,const char *val);
+  void setVariable(const char *name,double val);
+  void clearVariables();
+
+  std::string evalString(const char *str,xmlNodePtr=NULL);
+//  ... evalNodeset(const char *str,xmlNodePtr=NULL);
+  xmlNodePtr evalNode(const char *str,xmlNodePtr=NULL);
+
+  // TODO?! var-arg  functions ('s_sprintf')
+private:
+  XPathContext(const XPathContext &);
+  const XPathContext &operator=(const XPathContext &);
+  
+  xmlXPathContextPtr ctxt;
+};
+
+#endif
