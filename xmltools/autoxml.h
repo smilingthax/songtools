6cafab93fecadfad05bd34f5a93bc2a1d2c22dfa
smilingthax <smilingthax@c3327504-1e54-0410-be8d-d5a36682d75c>
2012-02-19 11:50:45 +0000

    added c++11 support

--- xmltools/autoxml.h
+++ xmltools/autoxml.h
@@ -3,6 +3,54 @@
 
 #include <libxml/tree.h>
 
+#if defined(__GXX_EXPERIMENTAL_CXX0X__)||(__cplusplus>=201103L)
+#include <memory>
+
+class unique_xmlFree { // {{{
+public:
+  unique_xmlFree()=default;
+//  explicit unique_xmlFree(void *data) : ptr(data) {}
+  explicit unique_xmlFree(xmlChar *data) : ptr(data) {}
+
+  inline operator const char *() const { return (const char *)ptr.get(); }
+//  inline xmlChar *release() { return ptr.release(); } // or: void *
+  inline void reset(xmlChar *data=NULL) { ptr.reset(data); }
+
+private:
+  struct xmlFree_deleter {
+    void operator()(xmlChar *data) {
+      xmlFree(data);
+    }
+  };
+  std::unique_ptr<xmlChar,xmlFree_deleter> ptr; // or:  void *
+};
+// }}}
+
+class unique_xmlDoc { // {{{
+public:
+  unique_xmlDoc()=default;
+  explicit unique_xmlDoc(xmlDocPtr doc) : ptr(doc) {}
+
+  inline operator xmlDocPtr() { return ptr.get(); }
+  inline xmlDocPtr operator->() const { return ptr.get(); }
+  inline xmlDocPtr release() { return ptr.release(); }
+  inline void reset(xmlDocPtr doc=NULL) { ptr.reset(doc); }
+
+private:
+  struct xmlDoc_deleter {
+    void operator()(xmlDocPtr doc) {
+      xmlFreeDoc(doc);
+    }
+  };
+  std::unique_ptr<xmlDoc,xmlDoc_deleter> ptr;
+};
+// }}}
+
+#define _A_X_F_DEPR  __attribute__((deprecated))
+#else
+#define _A_X_F_DEPR
+#endif
+
 class auto_xmlFree { // {{{
 public:
   explicit auto_xmlFree() : data(NULL) {}
@@ -26,7 +74,7 @@ private:
   const auto_xmlFree &operator=(const auto_xmlFree &);
 
   void *data;
-};
+} _A_X_F_DEPR;
 // }}}
 
 class auto_xmlDoc { // {{{
@@ -36,7 +84,7 @@ public:
   ~auto_xmlDoc() {
     xmlFreeDoc(doc);
   }
-  operator xmlDocPtr&() {
+  operator xmlDocPtr() {
     return doc;
   }
   const xmlDocPtr &operator->() const {
@@ -58,7 +106,9 @@ private:
   const auto_xmlDoc &operator=(const auto_xmlDoc &);
 
   xmlDocPtr doc;
-};
+} _A_X_F_DEPR;
 // }}}
+
+#undef _A_X_F_DEPR
 
 #endif
--- xmltools/autoxslt.h
+++ xmltools/autoxslt.h
@@ -3,6 +3,56 @@
 
 #include <libxslt/xsltutils.h>
 
+#include <libxslt/transform.h>
+
+#if defined(__GXX_EXPERIMENTAL_CXX0X__)||(__cplusplus>=201103L)
+#include <memory>
+
+class unique_xsltStylesheet { // {{{
+public:
+  unique_xsltStylesheet()=default;
+  explicit unique_xsltStylesheet(xsltStylesheetPtr style) : ptr(style) {}
+
+  inline operator xsltStylesheetPtr() const { return ptr.get(); }
+  inline xsltStylesheetPtr operator->() const { return ptr.get(); }
+  inline xsltStylesheetPtr release() { return ptr.release(); }
+  inline void reset(xsltStylesheetPtr style=NULL) { ptr.reset(style); }
+
+private:
+  struct xsltStylesheet_deleter {
+    void operator()(xsltStylesheetPtr style) {
+      xsltFreeStylesheet(style);
+    }
+  };
+  std::unique_ptr<xsltStylesheet,xsltStylesheet_deleter> ptr;
+};
+// }}}
+
+class unique_xsltTransform { // {{{
+public:
+  unique_xsltTransform()=default;
+  explicit unique_xsltTransform(xsltTransformContextPtr ctxt) : ptr(ctxt) {}
+
+  inline operator xsltTransformContextPtr() const { return ptr.get(); }
+  inline xsltTransformContextPtr operator->() const { return ptr.get(); }
+  inline xsltTransformContextPtr release() { return ptr.release(); } 
+  inline void reset(xsltTransformContextPtr ctxt=NULL) { ptr.reset(ctxt); }
+
+private:
+  struct xsltTransformContextPtr_deleter {
+    void operator()(xsltTransformContextPtr ctxt) {
+      xsltFreeTransformContext(ctxt);
+    }
+  };
+  std::unique_ptr<xsltTransformContext,xsltTransformContextPtr_deleter> ptr;
+};
+// }}}
+
+#define _A_X_F_DEPR  __attribute__((deprecated))
+#else
+#define _A_X_F_DEPR
+#endif
+
 class auto_xsltStylesheet { // {{{
 public:
   explicit auto_xsltStylesheet() : style(NULL) {}
@@ -32,10 +82,8 @@ private:
   const auto_xsltStylesheet &operator=(const auto_xsltStylesheet &);
 
   xsltStylesheetPtr style;
-};
+} _A_X_F_DEPR;
 // }}}
-
-#include <libxslt/transform.h>
 
 class auto_xsltTransform { // {{{
 public:
@@ -66,7 +114,9 @@ private:
   const auto_xsltTransform &operator=(const auto_xsltTransform &);
 
   xsltTransformContextPtr ctxt;
-};
+} _A_X_F_DEPR;
 // }}}
+
+#undef _A_X_F_DEPR
 
 #endif
