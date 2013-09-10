b5d83d263c4d531f90a28ad2a975cf5441c0b202
smilingthax <smilingthax@c3327504-1e54-0410-be8d-d5a36682d75c>
2013-09-10 23:40:41 +0000

    factored out config.mk

---
+++ trunk/xsltlib/config.mk
@@ -0,0 +1,19 @@
+#CXX=/home/thobi/dls/gstlfilt/gfilt
+#CXX=g++-4.4
+#CXX=g++-4.7
+#CFLAGS+=-O3 -funroll-all-loops -finline-functions -Wall -g
+CFLAGS+=-Wall -g
+CXXFLAGS+=
+LDFLAGS+=-g
+CPPFLAGS=$(CFLAGS) $(FLAGS)
+
+PKG_CONFIG:=pkg-config
+
+-include ../local.mk
+
+ifneq "$(PACKAGES)" ""
+  # these must be last in link ordering
+  CPPFLAGS+=$(shell $(PKG_CONFIG) --cflags $(PACKAGES))
+  LDFLAGS+=$(shell $(PKG_CONFIG) --libs $(PACKAGES))
+endif
+
