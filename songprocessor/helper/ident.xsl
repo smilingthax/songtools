smilingthax/songtools

commit 8a05918d75e029b10b41bb771c8c5eb53575b4eb

impress: use style (fake-)inheritance, use paragraph style for xlang

songprocessor/impress.xsl
@@ -280,8 +280,46 @@
  <xsl:template match="office:automatic-styles" mode="_output_odp_content">
    <xsl:param name="style_nodes" select="."/>
    <xsl:copy>
-     <xsl:copy-of select="@*|node()"/>
+     <xsl:copy-of select="@*"/>
+     <xsl:apply-templates select="node()" mode="_output_odp_content"/>
      <xsl:copy-of select="$style_nodes"/>
+   </xsl:copy>
+ </xsl:template>
+
+ <!-- {{{ FUNCTION func:_ooc_collect_parents_self(node) -->
+ <func:function name="func:_ooc_collect_parents_self"> <!-- function, to be able to return a nodeset of input nodes w/o id(...) tricks -->
+   <xsl:param name="node"/>
+   <xsl:choose>
+     <xsl:when test="$node/@copy-parent">
+       <xsl:variable name="parent" select="$node/preceding-sibling::style:style[@style:name=$node/@copy-parent]"/> <!-- parent must be defined earlier, because nodeset will be enumerated in document order! -->
+       <xsl:if test="count($parent|$node) != 2 or $parent/@style:family != $node/@style:family">
+         <xsl:message terminate="yes">Bad copy-parent</xsl:message>
+       </xsl:if>
+       <func:result select="func:_ooc_collect_parents_self($parent)|$node"/>
+     </xsl:when>
+     <xsl:otherwise>
+       <func:result select="$node"/>
+     </xsl:otherwise>
+   </xsl:choose>
+ </func:function>
+ <!-- }}} -->
+
+ <xsl:template match="office:automatic-styles/style:style" mode="_output_odp_content">
+   <xsl:copy>
+     <xsl:choose>
+       <xsl:when test="@copy-parent">
+         <xsl:copy-of select="@*[not(name()='copy-parent')]"/>
+         <xsl:for-each select="exsl:group-by-name(func:_ooc_collect_parents_self(.)/*)">
+           <xsl:copy>
+             <xsl:copy-of select="*/@*"/>
+             <xsl:copy-of select="exsl:group-by-name(*/*)/*[last()]"/>
+           </xsl:copy>
+         </xsl:for-each>
+       </xsl:when>
+       <xsl:otherwise>
+         <xsl:copy-of select="@*|node()"/>
+       </xsl:otherwise>
+     </xsl:choose>
    </xsl:copy>
  </xsl:template>
 
@@ -668,25 +706,15 @@
      </xsl:call-template>
    </xsl:variable>
 
-   <text:p>
-     <xsl:copy-of select="$blockfmt/@*"/>
-     <xsl:choose>
-       <xsl:when test="@xlang">
-         <text:span text:style-name="Txlang">
-           <xsl:copy-of select="$indent"/>
-           <text:s text:c="4"/>
-           <xsl:apply-templates select="node()" mode="_sc_post">
-             <xsl:with-param name="ctxt" select="$ctxt"/>
-           </xsl:apply-templates>
-         </text:span>
-       </xsl:when>
-       <xsl:otherwise>
-         <xsl:copy-of select="$indent"/>
-         <xsl:apply-templates select="node()" mode="_sc_post">
-           <xsl:with-param name="ctxt" select="$ctxt"/>
-         </xsl:apply-templates>
-       </xsl:otherwise>
-     </xsl:choose>
+   <xsl:if test="not($blockfmt/@text:style-name)">
+     <xsl:message terminate="yes">text:style-name is required</xsl:message>
+   </xsl:if>
+
+   <text:p text:style-name="{concat($blockfmt/@text:style-name,func:if(@xlang,'-xlang'))}">
+     <xsl:copy-of select="$indent"/>
+     <xsl:apply-templates select="node()" mode="_sc_post">
+       <xsl:with-param name="ctxt" select="$ctxt"/>
+     </xsl:apply-templates>
    </text:p><xsl:value-of select="$nl"/>
 
    <xsl:choose>
@@ -855,6 +883,25 @@
      </xsl:when>
    </xsl:choose>
  </xsl:template>
+ <!-- }}} -->
+
+ <!-- {{{ FUNCTION exsl:group-by-name(nodeset) -->
+ <func:function name="exsl:group-by-name">
+   <xsl:param name="nodeset"/>
+   <xsl:variable name="hlp">
+     <xsl:for-each select="$nodeset">
+       <xsl:copy><xsl:value-of select="name()"/></xsl:copy>
+     </xsl:for-each>
+   </xsl:variable>
+   <xsl:variable name="ret">
+     <xsl:for-each select="set:distinct(exsl:node-set($hlp)/*)">
+       <xsl:copy>
+         <xsl:copy-of select="$nodeset[name()=current()]"/>
+       </xsl:copy>
+     </xsl:for-each>
+   </xsl:variable>
+   <func:result select="exsl:node-set($ret)/*"/>
+ </func:function>
  <!-- }}} -->
 
  <!-- {{{ FUNCTION func:default (value, default)  -  return $value, if it is not empty (node-set), otherwise $default -->

songprocessor/oo-template/content-1.xml
@@ -65,7 +65,7 @@
         <style:text-properties fo:font-family="StarSymbol" style:use-window-font-color="true" fo:font-size="45%"/>
       </text:list-level-style-bullet>
     </text:list-style>
-<!-- Our Styles -->   
+<!-- Our Styles -->
 <style:style style:name="dp3" style:family="drawing-page"><!-- our black slide -->
   <style:drawing-page-properties presentation:background-visible="true" presentation:background-objects-visible="false" draw:fill="solid" draw:fill-color="#000000" presentation:display-footer="false" presentation:display-page-number="false" presentation:display-date-time="false"/>
 </style:style>
@@ -80,12 +80,13 @@
   <style:paragraph-properties fo:line-height="95%"/>
   <style:paragraph-properties fo:margin-left="0cm" fo:margin-right="0cm" fo:text-indent="0cm">
     <style:tab-stops>
+      <style:tab-stop style:position="0.0cm"/>
       <style:tab-stop style:position="0.95cm"/>
       <style:tab-stop style:position="1.90cm"/>
       <style:tab-stop style:position="2.85cm"/>
     </style:tab-stops>
   </style:paragraph-properties>
-  <style:text-properties fo:font-size="32pt" fo:font-weight="bold"/>
+  <style:text-properties fo:font-size="32pt" fo:font-weight="bold" style:font-size-asian="32pt" style:font-weight-asian="bold"/>
 </style:style>
 <style:style style:name="P3" style:family="paragraph"> <!-- half height line for spacing -->
   <style:paragraph-properties fo:margin-left="0.6cm" fo:margin-right="0cm" fo:line-height="50%" fo:text-indent="0cm"/>
@@ -111,56 +112,37 @@
   <style:paragraph-properties fo:margin-left="0cm" fo:margin-right="0cm" fo:text-align="end" fo:text-indent="0cm"/>
   <style:text-properties fo:color="#00ae00" fo:font-size="40pt" fo:font-weight="bold" style:font-size-asian="18pt" style:font-size-complex="18pt"/>
 </style:style>
-<style:style style:name="Pvers" style:family="paragraph">
-  <style:paragraph-properties fo:line-height="95%"/>
-  <style:paragraph-properties fo:margin-left="1.2cm" fo:margin-right="0cm" fo:text-indent="-1.2cm">
-    <style:tab-stops>
-      <style:tab-stop style:position="0.0cm"/>
-      <style:tab-stop style:position="0.95cm"/>
-      <style:tab-stop style:position="1.90cm"/>
-      <style:tab-stop style:position="2.85cm"/>
-    </style:tab-stops>
-  </style:paragraph-properties>
-  <style:text-properties fo:font-size="32pt" fo:font-weight="bold"/>
+<style:style style:name="Pvers" style:family="paragraph" copy-parent="P2">
+  <style:paragraph-properties fo:margin-left="1.2cm" fo:text-indent="-1.2cm"/>
 </style:style>
-<style:style style:name="Prefr" style:family="paragraph">
-  <style:paragraph-properties fo:line-height="95%"/>
-  <style:paragraph-properties fo:margin-left="3.10cm" fo:margin-right="0cm" fo:text-indent="-3.10cm">
-    <style:tab-stops>
-      <style:tab-stop style:position="0.0cm"/>
-      <style:tab-stop style:position="0.95cm"/>
-      <style:tab-stop style:position="1.90cm"/>
-      <style:tab-stop style:position="2.85cm"/>
-    </style:tab-stops>
-  </style:paragraph-properties>
-  <style:text-properties fo:font-size="32pt" fo:font-weight="bold"/>
+<style:style style:name="Prefr" style:family="paragraph" copy-parent="P2">
+  <style:paragraph-properties fo:margin-left="3.10cm" fo:text-indent="-3.10cm"/>
 </style:style>
-<style:style style:name="Pbridge" style:family="paragraph">
-  <style:paragraph-properties fo:line-height="95%"/>
-  <style:paragraph-properties fo:margin-left="4.30cm" fo:margin-right="0cm" fo:text-indent="-4.30cm">
-    <style:tab-stops>
-      <style:tab-stop style:position="0.0cm"/>
-      <style:tab-stop style:position="0.95cm"/>
-      <style:tab-stop style:position="1.90cm"/>
-      <style:tab-stop style:position="2.85cm"/>
-    </style:tab-stops>
-  </style:paragraph-properties>
-  <style:text-properties fo:font-size="32pt" fo:font-weight="bold"/>
+<style:style style:name="Pbridge" style:family="paragraph" copy-parent="P2">
+  <style:paragraph-properties fo:margin-left="4.30cm" fo:text-indent="-4.30cm"/>
 </style:style>
-<style:style style:name="Pending" style:family="paragraph">
-  <style:paragraph-properties fo:line-height="95%"/>
-  <style:paragraph-properties fo:margin-left="5.00cm" fo:margin-right="0cm" fo:text-indent="-5.00cm">
-    <style:tab-stops>
-      <style:tab-stop style:position="0.0cm"/>
-      <style:tab-stop style:position="0.95cm"/>
-      <style:tab-stop style:position="1.90cm"/>
-      <style:tab-stop style:position="2.85cm"/>
-    </style:tab-stops>
-  </style:paragraph-properties>
-  <style:text-properties fo:font-size="32pt" fo:font-weight="bold"/>
+<style:style style:name="Pending" style:family="paragraph" copy-parent="P2">
+  <style:paragraph-properties fo:margin-left="5.00cm" fo:text-indent="-5.00cm"/>
 </style:style>
-<style:style style:name="Txlang" style:family="text">
-  <style:text-properties fo:font-size="24pt" fo:font-style="italic" fo:color="#808000"/>
+<style:style style:name="P2-xlang" style:family="paragraph" copy-parent="P2">
+  <style:paragraph-properties fo:margin-left="0.9cm"/>
+  <style:text-properties fo:font-size="24pt" fo:font-style="italic" fo:color="#808000" style:font-size-asian="24pt" style:font-style-asian="italic"/>
+</style:style>
+<style:style style:name="Pvers-xlang" style:family="paragraph" copy-parent="Pvers">
+  <style:paragraph-properties fo:margin-left="2.1cm" fo:text-indent="-2.1cm"/>
+  <style:text-properties fo:font-size="24pt" fo:font-style="italic" fo:color="#808000" style:font-size-asian="24pt" style:font-style-asian="italic"/>
+</style:style>
+<style:style style:name="Prefr-xlang" style:family="paragraph" copy-parent="Prefr">
+  <style:paragraph-properties fo:margin-left="4.00cm" fo:text-indent="-4.00cm"/>
+  <style:text-properties fo:font-size="24pt" fo:font-style="italic" fo:color="#808000" style:font-size-asian="24pt" style:font-style-asian="italic"/>
+</style:style>
+<style:style style:name="Pbridge-xlang" style:family="paragraph" copy-parent="Pbridge">
+  <style:paragraph-properties fo:margin-left="5.20cm" fo:text-indent="-5.20cm"/>
+  <style:text-properties fo:font-size="24pt" fo:font-style="italic" fo:color="#808000" style:font-size-asian="24pt" style:font-style-asian="italic"/>
+</style:style>
+<style:style style:name="Pending-xlang" style:family="paragraph" copy-parent="Pending">
+  <style:paragraph-properties fo:margin-left="5.90cm" fo:text-indent="-5.90cm"/>
+  <style:text-properties fo:font-size="24pt" fo:font-style="italic" fo:color="#808000" style:font-size-asian="24pt" style:font-style-asian="italic"/>
 </style:style>
   </office:automatic-styles>
   <office:body>

songprocessor/oo-template/content.xml
@@ -65,7 +65,7 @@
         <style:text-properties fo:font-family="StarSymbol" style:use-window-font-color="true" fo:font-size="45%"/>
       </text:list-level-style-bullet>
     </text:list-style>
-<!-- Our Styles -->   
+<!-- Our Styles -->
 <style:style style:name="dp3" style:family="drawing-page"><!-- our black slide -->
   <style:drawing-page-properties presentation:background-visible="true" presentation:background-objects-visible="false" draw:fill="solid" draw:fill-color="#000000" presentation:display-footer="false" presentation:display-page-number="false" presentation:display-date-time="false"/>
 </style:style>
@@ -80,12 +80,13 @@
   <style:paragraph-properties fo:line-height="95%"/>
   <style:paragraph-properties fo:margin-left="0cm" fo:margin-right="0cm" fo:text-indent="0cm">
     <style:tab-stops>
+      <style:tab-stop style:position="0.0cm"/>
       <style:tab-stop style:position="0.95cm"/>
       <style:tab-stop style:position="1.90cm"/>
       <style:tab-stop style:position="2.85cm"/>
     </style:tab-stops>
   </style:paragraph-properties>
-  <style:text-properties fo:font-size="32pt" fo:font-weight="bold"/>
+  <style:text-properties fo:font-size="32pt" fo:font-weight="bold" style:font-size-asian="32pt" style:font-weight-asian="bold"/>
 </style:style>
 <style:style style:name="P3" style:family="paragraph"> <!-- half height line for spacing -->
   <style:paragraph-properties fo:margin-left="0.6cm" fo:margin-right="0cm" fo:line-height="50%" fo:text-indent="0cm"/>
@@ -111,56 +112,37 @@
   <style:paragraph-properties fo:margin-left="0cm" fo:margin-right="0cm" fo:text-align="end" fo:text-indent="0cm"/>
   <style:text-properties fo:color="#00ae00" fo:font-size="40pt" fo:font-weight="bold" style:font-size-asian="18pt" style:font-size-complex="18pt"/>
 </style:style>
-<style:style style:name="Pvers" style:family="paragraph">
-  <style:paragraph-properties fo:line-height="95%"/>
-  <style:paragraph-properties fo:margin-left="1.2cm" fo:margin-right="0cm" fo:text-indent="-1.2cm">
-    <style:tab-stops>
-      <style:tab-stop style:position="0.0cm"/>
-      <style:tab-stop style:position="0.95cm"/>
-      <style:tab-stop style:position="1.90cm"/>
-      <style:tab-stop style:position="2.85cm"/>
-    </style:tab-stops>
-  </style:paragraph-properties>
-  <style:text-properties fo:font-size="32pt" fo:font-weight="bold"/>
+<style:style style:name="Pvers" style:family="paragraph" copy-parent="P2">
+  <style:paragraph-properties fo:margin-left="1.2cm" fo:text-indent="-1.2cm"/>
 </style:style>
-<style:style style:name="Prefr" style:family="paragraph">
-  <style:paragraph-properties fo:line-height="95%"/>
-  <style:paragraph-properties fo:margin-left="3.10cm" fo:margin-right="0cm" fo:text-indent="-3.10cm">
-    <style:tab-stops>
-      <style:tab-stop style:position="0.0cm"/>
-      <style:tab-stop style:position="0.95cm"/>
-      <style:tab-stop style:position="1.90cm"/>
-      <style:tab-stop style:position="2.85cm"/>
-    </style:tab-stops>
-  </style:paragraph-properties>
-  <style:text-properties fo:font-size="32pt" fo:font-weight="bold"/>
+<style:style style:name="Prefr" style:family="paragraph" copy-parent="P2">
+  <style:paragraph-properties fo:margin-left="3.10cm" fo:text-indent="-3.10cm"/>
 </style:style>
-<style:style style:name="Pbridge" style:family="paragraph">
-  <style:paragraph-properties fo:line-height="95%"/>
-  <style:paragraph-properties fo:margin-left="4.30cm" fo:margin-right="0cm" fo:text-indent="-4.30cm">
-    <style:tab-stops>
-      <style:tab-stop style:position="0.0cm"/>
-      <style:tab-stop style:position="0.95cm"/>
-      <style:tab-stop style:position="1.90cm"/>
-      <style:tab-stop style:position="2.85cm"/>
-    </style:tab-stops>
-  </style:paragraph-properties>
-  <style:text-properties fo:font-size="32pt" fo:font-weight="bold"/>
+<style:style style:name="Pbridge" style:family="paragraph" copy-parent="P2">
+  <style:paragraph-properties fo:margin-left="4.30cm" fo:text-indent="-4.30cm"/>
 </style:style>
-<style:style style:name="Pending" style:family="paragraph">
-  <style:paragraph-properties fo:line-height="95%"/>
-  <style:paragraph-properties fo:margin-left="5.00cm" fo:margin-right="0cm" fo:text-indent="-5.00cm">
-    <style:tab-stops>
-      <style:tab-stop style:position="0.0cm"/>
-      <style:tab-stop style:position="0.95cm"/>
-      <style:tab-stop style:position="1.90cm"/>
-      <style:tab-stop style:position="2.85cm"/>
-    </style:tab-stops>
-  </style:paragraph-properties>
-  <style:text-properties fo:font-size="32pt" fo:font-weight="bold"/>
+<style:style style:name="Pending" style:family="paragraph" copy-parent="P2">
+  <style:paragraph-properties fo:margin-left="5.00cm" fo:text-indent="-5.00cm"/>
 </style:style>
-<style:style style:name="Txlang" style:family="text">
-  <style:text-properties fo:font-size="24pt" fo:font-style="italic" fo:color="#cc6700"/>
+<style:style style:name="P2-xlang" style:family="paragraph" copy-parent="P2">
+  <style:paragraph-properties fo:margin-left="0.9cm"/>
+  <style:text-properties fo:font-size="24pt" fo:font-style="italic" fo:color="#ff6600" style:font-size-asian="24pt" style:font-style-asian="italic"/>
+</style:style>
+<style:style style:name="Pvers-xlang" style:family="paragraph" copy-parent="Pvers">
+  <style:paragraph-properties fo:margin-left="2.1cm" fo:text-indent="-2.1cm"/>
+  <style:text-properties fo:font-size="24pt" fo:font-style="italic" fo:color="#ff6600" style:font-size-asian="24pt" style:font-style-asian="italic"/>
+</style:style>
+<style:style style:name="Prefr-xlang" style:family="paragraph" copy-parent="Prefr">
+  <style:paragraph-properties fo:margin-left="4.00cm" fo:text-indent="-4.00cm"/>
+  <style:text-properties fo:font-size="24pt" fo:font-style="italic" fo:color="#ff6600" style:font-size-asian="24pt" style:font-style-asian="italic"/>
+</style:style>
+<style:style style:name="Pbridge-xlang" style:family="paragraph" copy-parent="Pbridge">
+  <style:paragraph-properties fo:margin-left="5.20cm" fo:text-indent="-5.20cm"/>
+  <style:text-properties fo:font-size="24pt" fo:font-style="italic" fo:color="#ff6600" style:font-size-asian="24pt" style:font-style-asian="italic"/>
+</style:style>
+<style:style style:name="Pending-xlang" style:family="paragraph" copy-parent="Pending">
+  <style:paragraph-properties fo:margin-left="5.90cm" fo:text-indent="-5.90cm"/>
+  <style:text-properties fo:font-size="24pt" fo:font-style="italic" fo:color="#ff6600" style:font-size-asian="24pt" style:font-style-asian="italic"/>
 </style:style>
   </office:automatic-styles>
   <office:body>

commit 956956a899bac62ed51c3845562fcc20f293488d

impress: fix page-break calculation broken for xlang by 8a05918d75e029b10b41bb771c8c5eb53575b4eb

songprocessor/helper/ident.xsl
@@ -1,6 +1,6 @@
-<?xml version="1.0" encoding="iso-8859-1"?>
+<?xml version="1.0" encoding="utf-8"?>
 <xsl:stylesheet xmlns:xsl="http://www.w3.org/1999/XSL/Transform" version="1.0">
- <xsl:output method="xml" encoding="iso-8859-1"/>
+ <xsl:output method="xml" encoding="utf-8"/>
 
  <xsl:template match="@*|node()">
    <xsl:copy>

songprocessor/impress.xsl
@@ -568,7 +568,7 @@
      <xsl:attribute name="no"><!-- sum of lines up to here -->
        <xsl:value-of select="count($tr1[@text:style-name!='P3'])*2+
                              count($tr1[@text:style-name='P3'])-
-                             count($tr1[child::text:span[@text:style-name='Txlang']])*0.5"/>
+                             count($tr1[contains(@text:style-name,'-xlang')])*0.5"/>  <!-- (2-0.5)/2 = 0.75  (= 24pt/32pt) -->
      </xsl:attribute>
      <xsl:attribute name="self"><!-- number of lines contained inside this <page-cand>..</page-cand> -->
        <xsl:value-of select="func:if(node(),count(text:p[@text:style-name!='P3'])*2+